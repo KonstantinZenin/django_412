cafdf6c932248e3170fd305ebe2a33a262367043
Konstantin <Kostick_zenin@mai.ru>
2025-03-18 20:00:48 +0300

    feat: конвертер путей int и страничка master_detail

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import main
+from core.views import main, master_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main)
+    path('', main),
+    path("master/<int:master_id>/", master_detail),
 ]
