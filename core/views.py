d6fe22ed150e85f002d0a5971d896be9c903ead3
Konstantin <Kostick_zenin@mai.ru>
2025-03-20 21:29:51 +0300

    lesson_43: Добавлена демонстрация циклов в шаблоне test.html и обновлены данные для отображения.

--- core/views.py
+++ core/views.py
@@ -48,8 +48,12 @@ class Employee:
 
 def test(request):
     employee = Employee('Алевтина', True, True, 42, 100000, 'manager', ['Журналы про усы', 'Компьютерные игры', 'Пиво'])
-
     employee2 = Employee('Бородач', True, False, 25, 50000, 'master', ['Садоводство', 'Пиво', 'Компьютерные игры'])
+    employee3 = Employee("Барбарис", True, False, 30, 60000, 'master', ['Газонокосилки', 'Пиво', 'Стрельба из арбалета'])
+    employee4 = Employee("Сифон", True, True, 35, 70000, 'master', ['Брендовый шмот', 'Походы в ГУМ', 'Аниме'])
+
+    # список сотрудников
+    employees = [employee, employee2, employee3, employee4]
 
     context = {
         "string": "Мастер по усам",
@@ -58,5 +62,6 @@ def test(request):
         "dict": {"bestmaster": "Эльдар 'Бритва' Рязанов"},
         "employee": employee,
         "employee2": employee2,
+        "employees": employees,
     }
     return render(request, "test.html", context)
