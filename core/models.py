219c1bdee2523b5aac54488ddecc1f754a52f2ba
Konstantin <Kostick_zenin@mai.ru>
2025-04-08 19:41:12 +0300

    lesson_48: добавлена модель Order с полями для хранения информации о заказах и создана миграция для её добавления в базу данных; обновлены заметки по работе с ORM.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+"""
+CharField - строковое поле, которое может хранить текестовые данные.
+TextField - текстовое поле, которое может хранить большие объемы текстовых данных.
+IntegerField - целочисленное поле, которое может хранить целые числа.
+DateField - поле для хранения дат.
+BooleanField - логическое поле, которое может хранить значения True или False.
+JsonField - поле для хранения данных в формате JSON.
+"""
 
-# Create your models here.
+class Order(models.Model):
+    # id - генерируетс автоматически
+    client_name = models.CharField(max_length=100)
+    services = models.CharField(max_length=200)
+    master_id = models.IntegerField()
+    date = models.DateField()
+    status = models.CharField(max_length=50)
