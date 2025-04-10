afb4ffbd08e479bd9a6f4a2890f6dd5af4df8522
Konstantin <Kostick_zenin@mai.ru>
2025-04-10 21:02:22 +0300

    lesson_49: добавлена модель Service и связь многие ко многим с моделью Master; обновлены поля в модели Master и созданы миграции для изменения структуры базы данных.

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from doctest import master
+from email.mime import image
 from django.db import models
 from sqlalchemy import ForeignKey
 """
@@ -30,16 +31,28 @@ class Order(models.Model):
     status = models.CharField(max_length=50, choices=STATUS_CHOISES, default="not_approved")
     date_create = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now=True)
-    master = models.ForeignKey("Master", on_delete=models.SET_NULL, null=True)
+    # один ко многим
+    master = models.ForeignKey("Master", on_delete=models.SET_NULL, null=True, related_name="orders")
     appointment_date = models.DateTimeField(blank=True, null=True)
 
 
 class Master(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to="masters/")
+    photo = models.ImageField(upload_to="masters/", blank=True, null=True)
     phone = models.CharField(max_length=20)
-    addres = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
     email = models.EmailField(blank=True)
     experience = models.PositiveIntegerField()
+    # многие ко многим
+    services = models.ManyToManyField("Service", related_name="masters")
     is_active = models.BooleanField(default=True)
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.PositiveIntegerField(help_text="Время в минутах")
+    is_popular = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="services/", blank=True, null=True)
