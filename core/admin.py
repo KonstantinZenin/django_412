b73a9cd76541284b3ed7a85a22050a8da52d4a42
Konstantin <Kostick_zenin@mai.ru>
2025-05-22 19:17:42 +0300

    lesson_58: руссификация названия приложения в админке и настройка отображения MasterAdmin в admin.py

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,18 @@ from .models import Order, Master, Service, Review
 
 # Регистраци в одну строку
 admin.site.register(Order)
-admin.site.register(Master)
 admin.site.register(Service)
 admin.site.register(Review)
+
+
+# Создание кастомной админки для модели Master
+class MasterAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "phone", "experience", "is_active")
+    list_display_links = ("first_name", "last_name")
+    list_filter = ("is_active", "services", "experience")
+    search_fields = ("first_name", "last_name", "phone")
+    ordering = ("last_name", "first_name")
+    
+
+
+admin.site.register(Master, MasterAdmin)
