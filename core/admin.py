02e24aa346de832180ef13543393a860a3f79d51
Konstantin <Kostick_zenin@mai.ru>
2025-05-22 19:52:56 +0300

    lesson_58: кастомный столбец "Средний рейтинг мастера" со звёздами

--- core/admin.py
+++ core/admin.py
@@ -9,12 +9,40 @@ admin.site.register(Review)
 
 # Создание кастомной админки для модели Master
 class MasterAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "phone", "experience", "is_active")
+    list_display = (
+        "first_name",
+        "last_name",
+        "phone",
+        "experience",
+        "is_active",
+        "avg_rating_display",
+    )
     list_display_links = ("first_name", "last_name")
     list_filter = ("is_active", "services", "experience")
     search_fields = ("first_name", "last_name", "phone")
     ordering = ("last_name", "first_name")
-    
+
+
+# Какое название будет у поля в админке
+    @admin.display(description="Средняя оценка")
+    def avg_rating_display(self, obj) -> str:
+        """Форматированное отображение средней оценки"""
+        # Obj = Экземпляр модели Master
+        rating = obj.avg_rating()
+        if 0 < rating < 1:
+            return "🎃"
+        elif 1 <= rating < 2:
+            return "⭐"
+        elif 2 <= rating < 3:
+            return "⭐⭐"
+        elif 3 <= rating < 4:
+            return "⭐⭐⭐"
+        elif 4 <= rating < 5:
+            return "⭐⭐⭐⭐"
+        elif rating == 5:
+            return "⭐⭐⭐⭐⭐"
+        else:
+            return "❌"
 
 
 admin.site.register(Master, MasterAdmin)
